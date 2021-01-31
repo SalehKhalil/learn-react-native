1eafccd4a4a4ce52c13668eb8b454ba8850cb868
SalehKhalil <junior.khalil@hotmail.com>
2021-01-30 21:29:29 -0300

    perf👌 improvement view

--- src/Pages/Auth/Login/index.tsx
+++ src/Pages/Auth/Login/index.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
-import { View, TouchableHighlight, Text, Image } from 'react-native';
+import {
+    View,
+    TouchableHighlight,
+    Text,
+    Image,
+    KeyboardAvoidingView,
+    Platform,
+    ScrollView,
+} from 'react-native';
 
 import Input from '../../../Components/Input'
 
@@ -8,29 +16,40 @@ import styles from './styles';
 
 export default function Login() {
     return (
-        <View style={styles.container}>
-            <Image
-                source={CatPng}
-                resizeMode='stretch'
-                style={styles.catImg}
-            />
-            <Input
-                placeholder="E-mail"
-                icon="mail"
-                style={styles.emailInput}
-                placeholderTextColor="#666360"
-            />
-            <Input
-                placeholder="Senha"
-                icon="lock"
-                style={styles.passwordInput}
-                placeholderTextColor="#666360"
-            />
-            <TouchableHighlight
-                style={styles.enterButton}
+        <KeyboardAvoidingView
+            behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+            style={{ flex: 1 }}
+            enabled
+        >
+            <ScrollView
+                keyboardShouldPersistTaps="handled"
+                contentContainerStyle={{ flex: 1 }}
             >
-                <Text style={styles.enterButtonText}>Entrar</Text>
-            </TouchableHighlight>
-        </View>
+                <View style={styles.container}>
+                    <Image
+                        source={CatPng}
+                        resizeMode='stretch'
+                        style={styles.catImg}
+                    />
+                    <Input
+                        placeholder="E-mail"
+                        icon="mail"
+                        style={styles.emailInput}
+                        placeholderTextColor="#666360"
+                    />
+                    <Input
+                        placeholder="Senha"
+                        icon="lock"
+                        style={styles.passwordInput}
+                        placeholderTextColor="#666360"
+                    />
+                    <TouchableHighlight
+                        style={styles.enterButton}
+                    >
+                        <Text style={styles.enterButtonText}>Entrar</Text>
+                    </TouchableHighlight>
+                </View>
+            </ScrollView>
+        </KeyboardAvoidingView>
     );
 }
--- src/Pages/Auth/Login/styles.ts
+++ src/Pages/Auth/Login/styles.ts
@@ -4,8 +4,7 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         alignItems: 'center',
-        justifyContent: 'flex-start',
-        marginTop: 50,
+        justifyContent: 'center',
     },
     emailInput: {
         backgroundColor: '#232129',
